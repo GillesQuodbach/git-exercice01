89b2f68901bc7f39a3f20dbea3e458f27931bd65
Gilles <quodbach.gilles@gmail.com>
2024-01-03 10:13:00 +0100

    exercice terminé

--- src/gitExercice01/GitExo01.java
+++ src/gitExercice01/GitExo01.java
@@ -5,41 +5,73 @@ import java.util.*;
 public class GitExo01 {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
 		boolean reload = true;
-		Scanner scanner = new Scanner(System.in);
+
 		System.out.println("Bienvenue au PMU !");
+		while (reload) {
+			boolean horseValidNumber = true;
+			while (horseValidNumber) {
+				// Nombre de chevaux au départ
+				System.out.println("Nombre de chevaux (12 - 20)");
+				int userResponseHowManyHorses = scan.nextInt();
+				System.out.println(userResponseHowManyHorses);
 
-		System.out.println("Combien de chevaux son au depart de la course ? (12 - 20)");
+				if (userResponseHowManyHorses < 12 || userResponseHowManyHorses > 20) {
+					System.out.println("Vous devez choisir un chiffre entre 12 et 20");
+				} else {
+					horseValidNumber = false;
+					// Liste des chevaux
+					ArrayList<Integer> horsesList = new ArrayList<>();
+					// Remplissage de la liste des chevaux
+					for (int i = 0; i < userResponseHowManyHorses; i++) {
+//						int horseNumber = 1 + (int) (Math.random() * ((userResponseHowManyHorses - 1) + 1));
+//						horsesList.add(horseNumber);
+						horsesList.add(i + 1);
+					}
+					System.out.println(horsesList);
+					Collections.shuffle(horsesList);
+					System.out.println(horsesList);
+					// Type de jeu
+					System.out.println("Voulez-vous jouer au tiercé (1), au quarté (2) ou au quinté(5)");
+					horseValidNumber = false;
+					int userResponseWichGame = scan.nextInt();
+					System.out.println(userResponseWichGame);
 
-		// Nombre de chevaux au départ
-		int userResponseHowManyHorses = scanner.nextInt();
-		System.out.println(userResponseHowManyHorses);
+					if (userResponseWichGame == 1) {
+						// imprimmé les 3 premiers chevaux
+						System.out.println("L'arrivé du tièrcé est: " + horsesList.get(0) + " " + horsesList.get(1)
+								+ " " + horsesList.get(2));
+					} else if (userResponseWichGame == 2) {
+						// imprimmé les 4 premiers chevaux
+						System.out.println("L'arrivé du tièrcé est: " + horsesList.get(0) + " " + horsesList.get(1)
+								+ " " + horsesList.get(2) + " " + horsesList.get(3));
+					} else if (userResponseWichGame == 3) {
+						// imprimmé les 5 premiers chevaux
+						System.out.println("L'arrivé du tièrcé est: " + horsesList.get(0) + " " + horsesList.get(1)
+								+ " " + horsesList.get(2) + " " + horsesList.get(3) + " " + horsesList.get(4));
 
-		// Type de jeu
-		System.out.println("Voulez-vous jouer au tiercé (1), au quarté (2) ou au quinté(5)");
+					}
 
-		int userResponseWichGame = scanner.nextInt();
-		System.out.println(userResponseWichGame);
+				}
+			}
+			scan.nextLine();
 
-		// Liste des chevaux
-		ArrayList<Integer> horsesList = new ArrayList<>();
+			System.out.println("Rejouer ? Y/N");
+			String playerResponse = scan.nextLine();
+			if (playerResponse.equals("y")) {
+				reload = true;
+				System.out.println("C'est parti !!!!");
+			} else {
+				reload = false;
+				System.out.println("A bientot");
+			}
 
-		// Remplissage de la liste des chevaux
-		for (int i = 0; i < userResponseHowManyHorses; i++) {
-			int horseNumber = 1 + (int) (Math.random() * ((userResponseHowManyHorses - 1) + 1));
-			horsesList.add(horseNumber);
 		}
-		
-		if (userResponseWichGame == 1) {
-			// imprimmé les 3 premiers chevaux
-			System.out.println("L'arrivé du tièrcé est: " + horsesList.get(0) + " " + horsesList.get(1) + " " + horsesList.get(2) );
-		} else if (userResponseWichGame == 2) {
-			// imprimmé les 4 premiers chevaux
-		} else if (userResponseWichGame == 3) {
-			// imprimmé les 5 premiers chevaux
-		}
-		
-		System.out.println(horsesList);
+
+		scan.close();
+
 	}
 
 }
